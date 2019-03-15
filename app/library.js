47fd898d34338a00c0cc889c812e2cc3dee4d166
HeatherJLewis <heather@heatherlewis.co.uk>
2019-03-15 14:20:49 +0000

    All tasks completed and tests working

--- app/library.js
+++ app/library.js
@@ -1,14 +1,40 @@
-let books = [];
+let books = [
+    {
+        title: "Dont Make Me Think",
+        author: "Steve Krug",
+        isbn: 123,
+        available: true
+    },
+    {
+        title: "Site Reliability Engineering",
+        author: "Niall Richard Murphy",
+        isbn: 456,
+        available: true
+    },
+    {
+        title: "Fantastic Mr Fox",
+        author: "Roald Dahl",
+        isbn: 789,
+        available: false
+    }
+];
 
 function getAllBooks() {
-    
     return books;
 }
 
 function getAllAvailableBooks() {
-}
+        const availableBooks = books.filter(book => book.available === true);
+        return availableBooks;
+};
+
+function checkoutBook(isbn) {
+        const checkoutABook = books.filter(book => book.isbn === isbn)
+        return checkoutABook
+};
 
 module.exports = {
     getAllBooks,
-    getAllAvailableBooks
+    getAllAvailableBooks,
+    checkoutBook
 };
--- test/library.test.js
+++ test/library.test.js
@@ -1,17 +1,52 @@
 const library = require('../app/library');
 
 test('Check we have all books and stocked correctly', () => {
-  let expectedBooksInLibrary = [{
-
-  }];
-
+  let expectedBooksInLibrary = [
+    {
+        title: "Dont Make Me Think",
+        author: "Steve Krug",
+        isbn: 123,
+        available: true
+    },
+    {
+        title: "Site Reliability Engineering",
+        author: "Niall Richard Murphy",
+        isbn: 456,
+        available: true
+    },
+    {
+        title: "Fantastic Mr Fox",
+        author: "Roald Dahl",
+        isbn: 789,
+        available: false
+    }
+  ];
   expect(library.getAllBooks()).toEqual(expectedBooksInLibrary);
 });
 
 test('Check available books', () => {
   let availableBooksInLibrary = [{
-
-  }];
+    title: "Dont Make Me Think",
+    author: "Steve Krug",
+    isbn: 123,
+    available: true
+},
+{
+    title: "Site Reliability Engineering",
+    author: "Niall Richard Murphy",
+    isbn: 456,
+    available: true
+}];
 
   expect(library.getAllAvailableBooks()).toEqual(availableBooksInLibrary);
 });
+
+test('Checkout a book', () => {
+  let bookToCheckout = [{
+    title: "Fantastic Mr Fox",
+    author: "Roald Dahl",
+    isbn: 789,
+    available: false
+  }];
+  expect(library.checkoutBook(789)).toEqual(bookToCheckout);
+})
